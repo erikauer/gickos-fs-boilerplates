b587460bb6bc5b593bc7b522ec296c9125a07ee9
erikauer <erik.auer@bytesource.net>
2016-09-08 23:30:20 +0200

    #2 update structure.js test

--- test/structure.js
+++ test/structure.js
@@ -5,6 +5,6 @@ chai.should();
 
 describe('structure test suite',()=>{
   it('should return the correct structure',()=>{
-    customer.toString().should.equal(['01_meetings', '02_documentatoin', '03_code', '04_misc'].toString());
+    customer.toString().should.equal(['01_meetings', '02_documentation', '03_code', '04_misc'].toString());
   })
 });
